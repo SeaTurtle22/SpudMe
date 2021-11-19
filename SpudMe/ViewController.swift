203517322951474eb34b7ee415a550ca329ffb34
mustachio128 <94639819+mustachio128@users.noreply.github.com>
2021-11-19 11:44:15 -0700

    message

--- SpudMe/ViewController.swift
+++ SpudMe/ViewController.swift
@@ -6,9 +6,29 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,PHPickerViewControllerDelegate{
     @IBOutlet var chosephoto: UIButton!
+    @IBAction func chosephoto(_ sender: Any) {
+    var config = PHPickerConfiguration(photoLibrary: .shared())
+        config.selectionLimit = 1
+        config.filter = .images
+        let vc = PHPickerViewController(configuration:config)
+        vc.delegate = self
+    present(vc, animated: true)
+    }
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        results.forEach { result in
+            result.itemProvider.loadObject(ofClass: UIImage.self) {reading, error in
+                guard let image = reading as? UIImage, error == nil else{
+                    return
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
